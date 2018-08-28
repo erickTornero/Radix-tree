b01340504a659098441032c6bd70b85bed8de520
erickTorneroT <erickdeivy01@gmail.com>
2018-08-28 16:31:16 -0500

    Fix #2, added data structure for Files & docs and delete isleaf

--- Radix_trie.hpp
+++ Radix_trie.hpp
@@ -16,35 +16,47 @@ int get_maximum_similariy(const std::string & s1, const std::string & s2){
     }
     return c;
 }
+struct doc_type{
+    std::string file_direction;
+    int key;
+    doc_type(std::string f = "", int k = 0):file_direction(f), key(k){};
+};
 class Node_radix{
     private:
-    
-    
+    std::vector<doc_type> documents; 
+
     int number_childrens;
-    int number_elements;
-    std::vector<int> keys;
+    int number_documents;
     std::string value;
     public: 
     Node_radix * father;
     std::vector<Node_radix* > childrens;
-    Node_radix(bool __l, Node_radix * f = nullptr):is_leaf(__l),number_elements(0),number_childrens(0), father(f){};
-    Node_radix(bool __l, std::string s, Node_radix * f = nullptr):is_leaf(__l),number_elements(0),number_childrens(0), value(s), father(f){};
-    void push_child(std::string s){
-        childrens.push_back(new Node_radix(true, s, this));
+    Node_radix(bool __l, Node_radix * f = nullptr):number_documents(0),number_childrens(0), father(f){};
+    Node_radix(bool __l, std::string s, Node_radix * f = nullptr):number_documents(0),number_childrens(0), value(s), father(f){};
+    void push_child(std::string s, doc_type doc){
+        Node_radix * my_node = new Node_radix(true, s, this);
+        my_node->push_document(doc);
+        childrens.push_back(my_node);
+        //documents.push_back(doc);
         number_childrens++;
     }
-    void push_child(Node_radix * node){
+    void push_child(Node_radix * node, doc_type doc){
         childrens.push_back(node);
+        //documents.push_back(doc);
         number_childrens++;
+    }
+    void push_document(doc_type doc){
+        this->documents.push_back(doc);
+        number_documents++;
     }
     std::string get_value(){return value;}
     void set_value(std::string s){value = s; }
-    int get_children(){return number_childrens;}
-    //bool is_leaf(){return is_leaf;}
-    bool is_leaf;
+    int get_number_children(){return number_childrens;}
+    int get_number_documents(){return number_documents;}
+
     int search_for_indx(std::string s, int & indx){
         int sim;
-        for(int i = 0; i < this->get_children(); i++){
+        for(int i = 0; i < this->get_number_children(); i++){
             sim = get_maximum_similariy(s, this->childrens[i]->get_value());
             if(sim > 0){
                 indx = i;
@@ -59,11 +71,11 @@ class Radix_tree{
     private:
     Node_radix * root;
    
-    void insert(const std::string & s, Node_radix * node){
+    void insert(const std::string & s, Node_radix * node, doc_type doc){
         if(s != ""){
             if(this->root ==nullptr){
                 root = new Node_radix(false);
-                root->push_child(s);
+                root->push_child(s, doc);
                 return;
             }
         
@@ -72,29 +84,30 @@ class Radix_tree{
             int sim = node->search_for_indx(s, index);
             if(sim > 0){
                 if(sim == node->childrens[index]->get_value().size())
-                    insert(s.substr(sim,s.size()-1),node->childrens[index]);
+                    insert(s.substr(sim,s.size()-1),node->childrens[index], doc);
                 else{
                     Node_radix * newInterNode = new Node_radix(false, node->childrens[index]->get_value().substr(0,sim),node);
-                    newInterNode->push_child(node->childrens[index]);
+                    newInterNode->push_child(node->childrens[index], doc);
                     
                     node->childrens[index]->set_value(node->childrens[index]->get_value().substr(sim,node->get_value().size()-1));
                     node->childrens[index]->father = newInterNode;
                     node->childrens[index] = newInterNode;
 
-                    insert(s.substr(sim, s.size()), newInterNode);
+                    std::string remaining = s.substr(sim, s.size());
+                    if(remaining != "")
+                        insert(s.substr(sim, s.size()), newInterNode, doc);
+                    else
+                        newInterNode->push_document(doc);
                 }
             }
             else{
-                node->push_child(s);   
+                node->push_child(s, doc);   
             }
-            /*if(!wasInsert){
-                node->push_child(s);                
-            }*/
         }
     }
     public:
-    void insert(const std::string & s){
-        insert(s, this->root);
+    void insert(const std::string & s, doc_type doc){
+        insert(s, this->root, doc);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main(){
     /*tree->insert("hola");
     tree->insert("holi");
     tree->insert("holac");*/
-    tree->insert("doge");
-    tree->insert("dog");
-    tree->insert("dogo");
-    tree->insert("doctor");
-    tree->insert("dacia");
-    tree->insert("holi");
+    tree->insert("doge", doc_type("File1", 23));
+    tree->insert("dog", doc_type("File1", 18));
+    tree->insert("dogo", doc_type("File2", 3));
+    tree->insert("doctor", doc_type("File3", 24));
+    tree->insert("dacia", doc_type("File3", 2));
+    tree->insert("holi", doc_type("File4", 32));
     int xx = 21;
 }
